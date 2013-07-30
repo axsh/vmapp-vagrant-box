14e2608d23baf0fe6bf95074e1097a97497737f9
Masahito Yoshida <masahito@axsh.net>
2013-07-30 20:54:44 +0900

    update vagrant to 1.2.7.

--- xexecscript.d/vagrant.sh
+++ xexecscript.d/vagrant.sh
@@ -14,5 +14,5 @@ configure_virtualbox ${chroot_dir}
 chroot $1 $SHELL <<'EOS'
   # Vagrant Downloads
   # - http://downloads.vagrantup.com/
-  rpm -ivh http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_$(arch).rpm
+  rpm -ivh http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/vagrant_1.2.7_$(arch).rpm
 EOS
