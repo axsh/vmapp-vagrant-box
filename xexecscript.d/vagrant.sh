b87ba4ae432b1eb8576d3e4ca25d30588dda5c7f
Masahito Yoshida <masahito@axsh.net>
2013-07-30 21:18:09 +0900

    downgrade vagrant to 1.2.4.

--- xexecscript.d/vagrant.sh
+++ xexecscript.d/vagrant.sh
@@ -14,5 +14,8 @@ configure_virtualbox ${chroot_dir}
 chroot $1 $SHELL <<'EOS'
   # Vagrant Downloads
   # - http://downloads.vagrantup.com/
-  rpm -ivh http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/vagrant_1.2.7_$(arch).rpm
+  # x 1.2.7
+  # x 1.2.6
+  # x 1.2.5
+  rpm -ivh http://files.vagrantup.com/packages/0219bb87725aac28a97c0e924c310cc97831fd9d/vagrant_1.2.4_$(arch).rpm
 EOS
